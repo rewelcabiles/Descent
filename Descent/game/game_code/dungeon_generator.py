30b6d8ac591d62b00a5ae3f6a7df7763482bd3b4
Rewel Cabiles <rewel.sicuan@gmail.com>
2019-03-16 21:43:53 +0800

    Added foundation code for A* pathfinding (Doesnt work yet)

--- Descent/game/game_code/dungeon_generator.py
+++ Descent/game/game_code/dungeon_generator.py
@@ -8,6 +8,7 @@ class Division():
 
     def create_blank_map(self, max_x, max_y):
         temp_map = []
+        self.grid = SquareGrid(max_x, max_y)
         for x in range(max_x):
             for y in range(max_y):
                 temp_map.append((x, y, 0))
@@ -61,6 +62,34 @@ class Division():
                     remain_stack.append(new_node)
             remain_stack.remove(node)
         for tiles in self.new_map:
+            if tiles[2] == 1:
+                self.grid.walls.append((tiles[0], tiles[1]))
             self.world.factory.create_tiles(tiles)
+        self.world.set_grid(self.grid)
 
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+        self.weights = {}
+        
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+    
+    def passable(self, id):
+        return id not in self.walls
+    
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x + y) % 2 == 0: results.reverse() # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
 
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
+    
+        
--- Descent/game/game_code/world.py
+++ Descent/game/game_code/world.py
@@ -1,7 +1,7 @@
 import json
 import random
 import Descent.game.game_code.factory as factory
-
+import heapq
 
 
 class World():
@@ -18,9 +18,11 @@ class World():
 			self.WORLD[key] = {}
 			self.COMPS[key] = 1 << iterator
 			iterator += 1
-		
 		self.entity_id_max = 9000
 		self.factory = factory.Factory(self)
+
+	def set_grid(self, grid):
+		self.grid = grid
 
 	def assign_entity_id(self):
 		while True:
@@ -76,3 +78,47 @@ class World():
 
 	def get_components_as_json(self):
 		return json.dumps(self.COMPS)
+
+
+
+class PriorityQueue:
+	def __init__(self):
+		self.elements = []
+	
+	def empty(self):
+		return len(self.elements) == 0
+	
+	def put(self, item, priority):
+		heapq.heappush(self.elements, (priority, item))
+	
+	def get(self):
+		return heapq.heappop(self.elements)[1]
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+    
+    return came_from, cost_so_far
