ee7146b6cc3f3b303bbb4732f026a43bd7f6acd0
Rewel Cabiles <rewel.sicuan@gmail.com>
2019-03-16 21:42:36 +0800

    Added a Messenger System to Systems and Server Utils

--- Descent/game/game_code/systems.py
+++ Descent/game/game_code/systems.py
@@ -1,8 +1,32 @@
 import random
 
+
+class MessageBoard():
+	def __init__(self):
+		self.message_queue = []
+		self.observers = []
+		
+	def add_to_queue(self, message):
+		self.message_queue.append(message)
+		self.notify_observers(message)
+
+	def register(self, observer):
+		self.observers.append(observer)
+		return observer 
+
+	def notify_observers(self, message):
+		for observer in self.observers:
+			observer(message)
+
+
 class Systems():
-	def __init__(self, world):
+	def __init__(self, world, message_board):
 		self.world = world
+		self.message_board = message_board
+		self.message_board.register(self.notified)
+
+	def notified(message):
+		pass
 
 	def add_player(self):
 		spawn_points = []
@@ -11,3 +35,5 @@ class Systems():
 				if self.world.WORLD["tile"][entity_id]["walkable"] == True:
 					spawn_points.append(entity_id)
 		return self.world.factory.create_player(random.choice(spawn_points))
+
+
