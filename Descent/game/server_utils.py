e65593dfee240b6f70f16283293739b65b6c9707
Rewel Cabiles <rewel.sicuan@gmail.com>
2019-03-26 21:17:41 +0800

    Added class archetypes, a new component, and general clean up

--- Descent/game/server_utils.py
+++ Descent/game/server_utils.py
@@ -9,6 +9,7 @@ import _thread
 import time
 import random
 
+
 class Game:
 	def __init__(self, level_id):
 		self.level_id = level_id
@@ -16,10 +17,10 @@ class Game:
 		self.message_board = systems.MessageBoard()
 		self.message_board.register(self.notify)
 		self.systems = systems.Systems(self.world, self.message_board)
-		self.generator  = dungeon_generator.Division(self.world)
+		self.generator = dungeon_generator.Division(self.world)
 		self.generator.division()
 		socketio.on_event('connect_world', self.send_world_data)
-		socketio.on_event("client_event", self.receive_events)        
+		socketio.on_event("client_event", self.receive_events)
 
 	def notify(self, message):
 		socketio.emit("new_packet", message)
@@ -29,21 +30,21 @@ class Game:
 
 	def add_new_player(self, username, entity_id):
 		data = {
-			"type":"new_connection",
+			"type": "new_connection",
 			"data": {
-				"entity_id" : entity_id,
+				"entity_id": entity_id,
 				"components": self.world.get_display_components(entity_id)
 			}
 		}
 		self.message_board.add_to_queue(data)
 
 	def send_world_data(self):
-		entity_id  = self.systems.add_player()
+		entity_id = self.systems.add_player()
 		self.world.add_new_player(current_user.username, entity_id)
 		socketio.emit("get_world_data", {
-				"world_data": self.world.get_world_as_json(),
-				"component_data": self.world.get_components_as_json(),
-				"player_id": self.world.players[current_user.username]
+			"world_data": self.world.get_world_as_json(),
+			"component_data": self.world.get_components_as_json(),
+			"player_id": self.world.players[current_user.username]
 			}, room=clients[current_user.username].sid)
 		self.add_new_player(current_user.username, entity_id)
 
@@ -57,7 +58,7 @@ class Game:
 
 class Server:
 	def __init__(self):
-		model_layer.create_component_collection()
+		model_layer.create_collections()
 		self.levels = {}
 		self.running = True
 		self.create_level()
@@ -66,17 +67,14 @@ class Server:
 		_thread.start_new_thread(self.threaded_update, ())
 		socketio.on_event('connected', self.sync_users)
 		socketio.on_event('disconnect', self.remove_connection)
-		
+
 	def create_level(self):
 		while True:
 			level_id = random.randint(1, 2000)
 			if level_id not in self.levels.keys():
 				break
 		self.levels[level_id] = Game(level_id)
-		model_layer.save_level(self.levels[level_id].world.WORLD, level_id);
-		
-
-
+		# model_layer.save_level(self.levels[level_id].world.WORLD, level_id)
 
 	def remove_connection(self):
 		print("Disconnected: " + current_user.username)
@@ -86,14 +84,14 @@ class Server:
 	def sync_users(self):
 		if current_user.is_authenticated:
 			clients[current_user.username] = Socket(request.sid, current_user.username)
-			socketio.emit('initial_user_info', 
-				{'username': current_user.username}, 
+			socketio.emit('initial_user_info', {
+				'username': current_user.username},
 				room=clients[current_user.username].sid)
 
 	def threaded_update(self):
 		FPS = 30
 		lastFrameTime = 0
-		while self.running == True:
+		while self.running is True:
 			currentTime = time.time()
 			dt = currentTime - lastFrameTime
 			lastFrameTime = currentTime
@@ -105,6 +103,7 @@ class Server:
 			sleepTime = 1./FPS - (currentTime - lastFrameTime)
 			if sleepTime > 0:
 				time.sleep(sleepTime)
+
 
 class Socket:
 	def __init__(self, sid, username):
